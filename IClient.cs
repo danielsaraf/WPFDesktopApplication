504d560aaa84216d5205381c409dfa67c0ee14f1
danielsaraf <57967538+danielsaraf@users.noreply.github.com>
2020-09-24 18:12:51 +0300

    Add files via upload

---
+++ IClient.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FlightSimulatorApp
+{
+    interface IClient
+    {
+        void Connect(string ip, string port);
+        bool IsConnected();
+
+        void Write(string command);
+
+        string Read();
+
+        void SetReceiveTimeout(int t);
+        void Disconnect();
+    }
+}
